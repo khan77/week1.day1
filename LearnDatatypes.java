a79da7a260f3450f2c7cfc9f71c06b4ab7368420
khan77 <khan.ihusan@gmail.com>
2022-04-16 23:17:46 +0530

    Add files via upload

---
+++ LearnDatatypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class LearnDatatypes {
+	//Primitive Data types = Car;
+	//Data type VaraiableName= Value;
+	public static void main(String[] args) {
+		
+	}
+	byte wheelpressure = 125;
+	short availablestock = 31000;
+	int mileage = 50;
+	long insurenum= 8908765457L;
+	float tyrewidh= 5.4908F;
+	double vechilespeed = 150.90;
+	char model= 'v';
+	boolean isreadytodrive= true;
+	
+
+}
